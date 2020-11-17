f01921ea2a7e4189333892de70e1d2b00d336544
alexpilotvdv <a41751@gmail.com>
2020-11-17 15:30:44 +0300

    alm fin2

--- scripts/main/zapis.php
+++ scripts/main/zapis.php
@@ -12,28 +12,31 @@ if ($user === false) {
 if ($user === true) {
     echo '<div class="container">';
     //выводим информацию по дню
-    if(isset($_GET['idday'])){$day_id=$_GET['idday'];}
-    if(isset($_POST['idday'])){$day_id=$_POST['idday'];}
+    if (isset($_GET['idday'])) {
+        $day_id=$_GET['idday'];
+    }
+    if (isset($_POST['idday'])) {
+        $day_id=$_POST['idday'];
+    }
     $sql = 'SELECT *
     FROM `'. BEZ_DBPREFIX .'day_for_flight`
     WHERE `day_id` = ' . $day_id . '';
     $res=mysqlQuery($sql);
-    if(mysql_num_rows($res) > 0){
-      $row = mysql_fetch_assoc($res);
-    echo'  <div class="row">
+    if (mysql_num_rows($res) > 0) {
+        $row = mysql_fetch_assoc($res);
+        echo'  <div class="row">
       <div class="col-sm">
     <div class="card">
     <h5 class="card-header">' . $row['day_data'] . '</h5>
     <div class="card-body">
-      <h5 class="card-title">Начало полетов: ' . substr($row['day_start'],-8) . '</h5>
-      <h5 class="card-title">Окончание полетов: ' . substr($row['day_end'],-8) . '</h5>
+      <h5 class="card-title">Начало полетов: ' . substr($row['day_start'], -8) . '</h5>
+      <h5 class="card-title">Окончание полетов: ' . substr($row['day_end'], -8) . '</h5>
       <h5 class="card-title">Планируемый максимальный налет: ' . $row['day_total_time'] . '</h5>
       <p class="card-text">Дополнительная информация: ' . $row['day_info'] . '</p>
       </div>
      </div>
     </div>
   </div>';
-
     }
 
     //выводим информацию по записавшимся
@@ -43,16 +46,16 @@ if ($user === true) {
     ON `rec_iduser` = `pr_id_reg`
     WHERE `rec_day` = ' . $day_id . '';
     $res=mysqlQuery($sql);
-    if(mysql_num_rows($res) > 0){
-      echo ' <div class="row">
+    if (mysql_num_rows($res) > 0) {
+        echo ' <div class="row">
         <div class="col-sm"><p>';
-       while($row = mysql_fetch_assoc($res)){
-         echo'<div class="alert alert-success" role="alert">
+        while ($row = mysql_fetch_assoc($res)) {
+            echo'<div class="alert alert-success" role="alert">
        ' . $row['pr_name'] . ' - ' . $row['rec_plan_nalet'] . ' минут.<br/>
        ' . $row['rec_info'] . '
          </div>';
-       }
-       echo'</div>
+        }
+        echo'</div>
            </div>';
     }
 
@@ -74,55 +77,55 @@ if ($user === true) {
         WHERE `rec_iduser` = ' . $user_id . '';
 
     $res=mysqlQuery($sql);
-    if(mysql_num_rows($res) > 0){
-      $tp_flag=1;
-    $row = mysql_fetch_assoc($res);
-    for($i=10;$i<190;$i=$i+10){
-      if($i==$row['rec_plan_nalet']){
-        echo'<option selected value="' . $i . '">' . $i . '</option>';
-      } else {
-        echo'<option value="' . $i . '">' . $i . '</option>';
-      }
+    if (mysql_num_rows($res) > 0) {
+        $tp_flag=1;
+        $row = mysql_fetch_assoc($res);
+        for ($i=10;$i<190;$i=$i+10) {
+            if ($i==$row['rec_plan_nalet']) {
+                echo'<option selected value="' . $i . '">' . $i . '</option>';
+            } else {
+                echo'<option value="' . $i . '">' . $i . '</option>';
+            }
+        }
+    } else {
+        $tp_flag=0;
+        //выведем просто форму
+        for ($i=10;$i<190;$i=$i+10) {
+            if ($i==40) {
+                echo'<option selected value="' . $i . '">' . $i . '</option>';
+            } else {
+                echo'<option value="' . $i . '">' . $i . '</option>';
+            }
+        }
     }
-  } else {
-    $tp_flag=0;
-    //выведем просто форму
-    for($i=10;$i<190;$i=$i+10){
-      if($i==40){
-        echo'<option selected value="' . $i . '">' . $i . '</option>';
-      } else {
-        echo'<option value="' . $i . '">' . $i . '</option>';
-      }
-    }
-  }
-  echo '</select></div>';
-  if($tp_flag==1){
-    echo'<input type = "hidden" name = "action" value = "edit_new">';
-    echo'<input type = "hidden" name = "id" value = "' . $row['rec_id'] . '">';
-    echo'  <div class="form-group">
+    echo '</select></div>';
+    if ($tp_flag==1) {
+        echo'<input type = "hidden" name = "action" value = "edit_new">';
+        echo'<input type = "hidden" name = "id" value = "' . $row['rec_id'] . '">';
+        echo'  <div class="form-group">
       <label>Информация</label>
       <textarea class="form-control" name="info"
         rows="3">' . $row['rec_info'] . '</textarea>
       </div>';
-  }
-  if($tp_flag==0){
-    echo'<input type = "hidden" name = "action" value = "add_new">';
-    echo'  <div class="form-group">
+    }
+    if ($tp_flag==0) {
+        echo'<input type = "hidden" name = "action" value = "add_new">';
+        echo'  <div class="form-group">
       <label>Информация</label>
       <textarea class="form-control" name="info"  rows="3"></textarea>
       </div>';
-  }
+    }
 
-  echo'<button type="submit" class="btn btn-primary">Сохранить</button>';
+    echo'<button type="submit" class="btn btn-primary">Сохранить</button>';
 
     echo ' </form>
             </div>
             </div>
             </div>';
-//обработаем данные формы и перезагрузим страницу
-if(isset($_POST['action'])){
-if($_POST['action']=='add_new' AND $tp_flag==0){
-  $sql='INSERT INTO
+    //обработаем данные формы и перезагрузим страницу
+    if (isset($_POST['action'])) {
+        if ($_POST['action']=='add_new' and $tp_flag==0) {
+            $sql='INSERT INTO
   `'. BEZ_DBPREFIX .'records`
   VALUES(
     "",
@@ -131,17 +134,16 @@ if($_POST['action']=='add_new' AND $tp_flag==0){
     ' . $_POST['minutes'] . ' ,
     0,
     "' . $_POST['info'] . '")';
-    $res=mysqlQuery($sql);
-    header('Location:'. BEZ_HOST .'?mode=zapis&idday=' . $day_id . '');
-} else if ($_POST['action']=='edit_new'){
-  $sql='UPDATE
+            $res=mysqlQuery($sql);
+            header('Location:'. BEZ_HOST .'?mode=zapis&idday=' . $day_id . '');
+        } elseif ($_POST['action']=='edit_new') {
+            $sql='UPDATE
   `'. BEZ_DBPREFIX .'records` SET
   `rec_plan_nalet` = ' . $_POST['minutes'] . ' ,
   `rec_info` = "' . $_POST['info'] . '"
   WHERE `rec_id` = ' . $_POST['id'] . '';
-$res=mysqlQuery($sql);
-header('Location:'. BEZ_HOST .'?mode=zapis&idday=' . $day_id . '');
-}
-}
-
+            $res=mysqlQuery($sql);
+            header('Location:'. BEZ_HOST .'?mode=zapis&idday=' . $day_id . '');
+        }
+    }
 }
